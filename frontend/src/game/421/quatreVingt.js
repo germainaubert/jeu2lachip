4db977ef5044b6ac2538dfb52a45046552f964c8
ReineDeLaNight <germain40k@hotmail.fr>
2021-01-21 13:34:27 +0100

    correction sur le dé

--- frontend/src/game/421/quatreVingt.js
+++ frontend/src/game/421/quatreVingt.js
@@ -5,14 +5,12 @@ import { HUD } from './HUD'
 (global).CANNON = require('cannon')
 
 export class QuatreVingt {
-    camera
     constructor(canvas, socket, engine, localPlayer, lobbyId, gameLeader) {
         this.canvas = canvas
         this.socket = socket
         this.scene = new BABYLON.Scene(engine)
-        // let gravityVector = new BABYLON.Vector3(0, -13, 0)
         var physicsPlugin = new BABYLON.CannonJSPlugin()
-        this.scene.enablePhysics(null, physicsPlugin)
+        this.scene.enablePhysics(new BABYLON.Vector3(0, -9.0, 0), physicsPlugin)
         this.gameLeader = gameLeader
         this.gameData = null
         this.players = null
@@ -23,6 +21,7 @@ export class QuatreVingt {
         this.forceMagnitude = 50;
         this.contactLocalRefPoint = BABYLON.Vector3.Zero();
         this.refreshIntervalId = null
+        this.ground = null
         if (this.gameLeader) {
             console.log("envoi du init421")
             this.socket.emit("init421", this.lobbyId)
@@ -31,71 +30,78 @@ export class QuatreVingt {
         this.basicInit()
     }
 
-    basicInit() {
+    async basicInit() {
+        // let physicsViewer = new BABYLON.Debug.PhysicsViewer();
+        // camera
         let camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 0, 0), this.scene)
         camera.setTarget(Vector3.Zero());
         camera.attachControl(this.canvas, true);
+        // light
         let light = new BABYLON.HemisphericLight("light1", new Vector3(0, 1, 0), this.scene);
         light.intensity = 0.7;
-        // let shadowGenerator = new BABYLON.ShadowGenerator(2048, light);
-
-        let ground = BABYLON.Mesh.CreateGround("ground1", 64, 64, 2, this.scene);
-        ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.8 }, this.scene)
-
-        // let physicsViewer = new BABYLON.Debug.PhysicsViewer();
-        let myMaterial = new BABYLON.StandardMaterial("myMaterial")
-        myMaterial.diffuseTexture = new BABYLON.Texture("http://localhost:3000/static/dice.jpg", this.scene)
+        // ground
+        this.ground = BABYLON.Mesh.CreateGround("ground1", 64, 64, 2, this.scene);
+        this.ground.physicsImpostor = new BABYLON.PhysicsImpostor(this.ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 1.0 }, this.scene)
+        // mesh options
+        const boxMat = new BABYLON.StandardMaterial("boxMat");
+        boxMat.diffuseTexture = new BABYLON.Texture("http://localhost:3000/static/421/longDice.jpg")
         let faceUV = [];
-        faceUV[0] = new BABYLON.Vector4(0, 0, 0.333, 0.5); //rear face
-        faceUV[1] = new BABYLON.Vector4(0.333, 0, 0.666, 0.5); //front face
-        faceUV[2] = new BABYLON.Vector4(0.666, 0, 0.999, 0.5); //right side
-        faceUV[3] = new BABYLON.Vector4(0, 0.5, 0.333, 1); //left side
-        faceUV[4] = new BABYLON.Vector4(0.333, 0.5, 0.666, 1);
-        faceUV[5] = new BABYLON.Vector4(0.666, 0.5, 0.999, 1);
-        //wrap set
+        faceUV[0] = new BABYLON.Vector4(0.0, 0.0, 0.16, 1.0); //rear face
+        faceUV[1] = new BABYLON.Vector4(0.16, 0.0, 0.32, 1.0); //front face
+        faceUV[2] = new BABYLON.Vector4(0.32, 0, 0.48, 1.0); //right side
+        faceUV[3] = new BABYLON.Vector4(0.48, 0, 0.64, 1.0); //left side
+        faceUV[4] = new BABYLON.Vector4(0.64, 0, 80, 1.0); 
+        faceUV[5] = new BABYLON.Vector4(0.80, 0, 0.96, 1.0); 
         let options = {
-            height: diceSize,
-            width: diceSize,
-            depth: diceSize,
+            height: 2,
+            width: 2,
+            depth: 2,
             faceUV: faceUV,
             wrap: true
         };
-        let diceSize = 2;
+        // let diceSize = 2;
+        // let physicsViewer = new BABYLON.Debug.PhysicsViewer();
         let diceMass = 5
-        let diceImpostorParams = { mass: diceMass, restitution: 0.7, friction: 1 };
-        // let diceMaterial = new BABYLON.StandardMaterial("boxMaterial");
-        // diceMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0);
-        for (let i = 0; i < 3; i++) {
-            let diceName = "dice:" + i
-            this.diceMeshes[i] = BABYLON.MeshBuilder.CreateBox(diceName, options, this.scene);
-            this.diceMeshes[i].position = new BABYLON.Vector3(4 * i, 4, 0);
-            // this.diceMeshes[i].material = diceMaterial;
+        let diceNumber = 3
+        let diceImpostorParams = { mass: diceMass, restitution: 0.3, friction: 1 };
+        // mesh import 
+        // let container = await BABYLON.SceneLoader.LoadAssetContainerAsync("http://localhost:3000/static/421/", "dice.gltf", this.scene)
+        // let mesh = container.meshes[0]
+        for (let i = 0; i < diceNumber; i++) {
+            let diceName = "dice:" + i + 1
+            this.diceMeshes[i] = BABYLON.MeshBuilder.CreateBox(diceName, options);
+            this.diceMeshes[i].position = new BABYLON.Vector3(4 * i, 1, 0);
+            this.diceMeshes[i].material = boxMat
             this.diceMeshes[i].physicsImpostor = new BABYLON.PhysicsImpostor(this.diceMeshes[i], BABYLON.PhysicsImpostor.BoxImpostor, diceImpostorParams, this.scene);
-            this.diceMeshes[i].material = myMaterial
-            // this.diceMeshes[i].cubeBody = this.diceMeshes[i].setPhysicsState({impostor: BABYLON.PhysicsEngine.BoxImpostor, mass:diceMass, restitution:0.7})
-            // shadowGenerator.addShadowCaster(this.diceMeshes[i])
+
             // physicsViewer.showImpostor(this.diceMeshes[i].physicsImpostor);
         }
     }
 
     update() {
         if (this.gameData.throwNotif) {
-            
+
             let randomVector = this.randomDice()
             this.socket.emit('diceVector', this.lobbyId, randomVector)
-               
-            this.gameData.throwNotif = false   
+
+            this.gameData.throwNotif = false
         }
         this.routerHUD()
     }
 
     randomDice() {
-        return new Vector3(randomNumber(), 5 + randomNumber(), randomNumber())
+        let vectorTab = []
+        for (let i = 0; i < 3; i++) {
+            vectorTab.push(new Vector3(randomNumber(), 2 + randomNumber(), randomNumber()))
+        }
+        return  vectorTab
     }
 
     impulse(vector) {
+        let i = 0
         for (let dice of this.diceMeshes) {
-            dice.physicsImpostor.applyImpulse((new Vector3(vector._x, vector._y, vector._z)).scale(10), dice.getAbsolutePosition().add(this.contactLocalRefPoint))
+            dice.physicsImpostor.applyImpulse((new Vector3(vector[i]._x, vector[i]._y, vector[i]._z)).scale(10), dice.getAbsolutePosition().add(this.contactLocalRefPoint))
+            i++
             // box.physicsImpostor.applyImpulse(impulseDirection.scale(impulseMagnitude), box.getAbsolutePosition().add(contactLocalRefPoint))
         }
     }
@@ -138,12 +144,12 @@ export class QuatreVingt {
                         flag = false
                         clearInterval(this.refreshIntervalId)
                     }
-                    
+
                 }
             }
-            
+
         }, 1000)
-        
+
         return results
     }
     getResult(dice) {
@@ -187,7 +193,7 @@ export class QuatreVingt {
 }
 
 function randomNumber() {
-    let random = Math.random() * 2
+    let random = Math.random()
     if (Math.random() < 0.5) {
         random = -random
     }
